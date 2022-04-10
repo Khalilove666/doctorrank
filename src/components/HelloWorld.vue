cfb09af7695b4f0633f76c2e8c6cf343d5009859
Khalilove666 <khalilove666@gmail.com>
2022-04-10 19:06:36 +0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container fluid>
+        <div>
+            <v-row>
+                <v-col cols="12" sm="6">
+                    <h1>Hello World</h1>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <h1>Doctor Rank is getting ready</h1>
+                </v-col>
+            </v-row>
+        </div>
+    </v-container>
+</template>
+
+<script setup lang='ts'>
+
+</script>
