a44b6cc5a299b28153af0532e6fc03e93335c045
Elgun <khalilove666@gmail.com>
2022-04-16 23:13:52 +0400

    language design done

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,16 +9,16 @@
                 indeterminate
             ></v-progress-linear>
             <div>
-                <h3>Login</h3>
-                <v-text-field v-model="username" class="mt-4" color="accent" label="Username or email" variant="outlined"
+                <h3>{{ $t('login') }}</h3>
+                <v-text-field v-model="username" class="mt-4" color="accent" :label="$t('username_or_email')" variant="outlined"
                               density="compact" hide-details></v-text-field>
-                <v-text-field v-model="password" class="mt-4" color="accent" label="Password" type="password" variant="outlined"
+                <v-text-field v-model="password" class="mt-4" color="accent" :label="$t('password')" type="password" variant="outlined"
                               density="compact" hide-details></v-text-field>
                 <div class="d-flex justify-end">
-                    <router-link to="/register" class="mt-1 text-decoration-underline text-accent">şifrəmi unutdum</router-link>
+                    <router-link to="/register" class="mt-1 text-decoration-underline text-accent">{{ $t('forgot_password') }}</router-link>
                 </div>
-                <v-btn @click="handleLogin()" :disabled="loading" class="mt-4 bg-accent text-white-text" block>DAXİL OL</v-btn>
-                <p class="mt-4">Digər daxil olma seçimləri</p>
+                <v-btn @click="handleLogin()" :disabled="loading" class="mt-4 bg-accent text-white-text" block>{{$t('btn_login')}}</v-btn>
+                <p class="mt-4">{{$t('login_options')}}</p>
                 <v-divider></v-divider>
                 <div class="mt-4 d-flex justify-space-between">
                     <v-btn
