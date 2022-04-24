8d950814069880a5a43ccd7814b8104daa6fbac7
Elgun <khalilove666@gmail.com>
2022-04-24 15:25:35 +0400

    doctor card done

---
+++ src/components/DoctorCard.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-card
+        class="mx-auto cursor-pointer"
+        rounded
+    >
+        <v-img
+            :src="evelyn"
+            cover
+            height="400"
+        ></v-img>
+        <v-card-title>
+            Evelyn Claire
+        </v-card-title>
+        <v-card-actions>
+            <v-chip
+                color="pink"
+                label
+                text-color="white"
+            >
+                <v-icon start icon="mdi-label"></v-icon>
+                Mama-ginekoloq
+            </v-chip>
+            <v-spacer></v-spacer>
+            <v-chip
+                v-ripple
+                color="accent"
+                variant="outlined"
+            >
+                <v-avatar left>
+                    <v-img :src="av_hsp"></v-img>
+                </v-avatar>
+                Avrasiya Hospital
+            </v-chip>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import evelyn from "../assets/img/evelyn.jpg";
+import av_hsp from "../assets/img/av_hsp.png";
+
+interface Doctor {
+    first_name: string,
+    last_name: string,
+    img: string,
+    profession: string,
+    hospital: [
+        name: string,
+        logo: string,
+    ],
+}
+
+interface Obj {
+    doctor: Doctor
+}
+
+const props = defineProps<Obj>()
+
+const show = ref(false);
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Doctors.vue
+++ src/pages/Doctors.vue
@@ -1,9 +1,55 @@
 <template>
-    <h1>All Doctor's Page</h1>
+    <div>
+        <h1>All Doctor's Page</h1>
+        <div class="v-row">
+            <div class="v-col-12 v-col-sm-6">
+                <v-text-field
+                    density="compact"
+                    variant="outlined"
+                    label="Search term"
+                    color="accent"
+                    hide-details
+                ></v-text-field>
+            </div>
+            <div class="v-col-12 v-col-sm-6">
+                <v-autocomplete
+                    v-model="values.selected"
+                    :items="items"
+                    density="compact"
+                    chips
+                    small-chips
+                    label="Solo"
+                    multiple
+                    clearable
+                    variant="outlined"
+                    color="accent"
+                    hide-details
+                ></v-autocomplete>
+            </div>
+        </div>
+        <div class="v-row">
+            <div class="v-col-12 v-col-sm-6 v-col-md-3" v-for="doctor in doctors" :key="doctor.id">
+                <DoctorCard/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import DoctorCard from "../components/DoctorCard.vue";
+import {reactive} from "vue";
 
+const values = reactive({
+    selected: ['foo', 'bar']
+});
+const items = ['foo', 'bar', 'fizz', 'buzz'];
+
+const doctors = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+]
 </script>
 
 <style scoped>
