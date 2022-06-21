79682ae580c4b4feb0c3bb56678d6d278d98f065
Elgun <khalilove666@gmail.com>
2022-06-21 21:02:08 +0400

    rules updated

--- src/composables/rules.ts
+++ src/composables/rules.ts
@@ -1,10 +1,22 @@
-import {computed} from "vue";
-
-
 export function useRules() {
 
-    const ruleEmpty = computed((value) => !!value || "rule_empty");
-    const ruleNumber = computed((value) => /^[0-9]*[.]{0,1}[0-9]*$/g.test(value) || "rule_number");
+    function empty(value: string) {
+        return !!value || "rule_empty"
+    }
 
-    return {ruleEmpty, ruleNumber};
+    function number(value: string) {
+        return /^[0-9]*[.]{0,1}[0-9]*$/g.test(value) || "rule_number";
+    }
+
+    function email(value: string) {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'rule_email';
+    }
+
+    function password(value: string) {
+        const pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-/.]).{8,}$/
+        return pattern.test(value) || 'rule_password';
+    }
+
+    return {empty, number, email, password};
 }
